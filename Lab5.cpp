5c214cc6f35c4c7e356fbb6e5ef707ea846fb70b
alinaast <55184585+alinaast@users.noreply.github.com>
2019-10-20 14:46:07 +0300

    Add files via upload

---
+++ Lab5.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	int a;
+	cin >> a;
+	a = a % 60;
+	cout << a;
+	return 0;
+}
---
+++ Lab6.4.cpp
@@ -0,0 +1,14 @@
+﻿#include <iostream>
+using namespace std;
+int main() {
+	double a, b, c, s1, s2;
+	int n, s;
+	cin >> a >> b >> c;
+	s1 = a * b;
+	s2 = c * c;
+	n = s1 / s2;
+	s = s1 - s2 * n;
+	cout << n << endl;
+	cout << s;
+	return 0;
+}
